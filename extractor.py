130c65bd62c506ecea997b99379c0a2b7d519906
Rahat Mahbub <rahatm1@uvic.ca>
2017-03-30 17:16:07 -0700

    Handles both files

--- extractor.py
+++ extractor.py
@@ -1,29 +1,43 @@
-import re
+import re, sys
 from subprocess import call
 
-file = open("fiftygrids.sudoku", "r")
-filename = None
-currentFile = None
-output = ""
 
-for line in file:
-    if re.match("^Grid.*", line):
-        if(currentFile != None):
+def main(argv):
+    if (len(argv) < 1):
+        print("Incorrect Args")
+        return
+    infile = argv[0]
+
+    file = open(infile, "r")
+    currentFile = None
+    puzzle = 1
+    output = ""
+
+    for line in file:
+        if re.match("^Grid.*", line): #handle Euler format
+            filename = line
+            filename = filename.strip().replace(" ", "")
+            currentFile = open(filename +".txt", "w")
+            for _ in range(9):
+                currentFile.write(file.next())
             currentFile.close()
-            call(["javac", "Sud2sat.java"])
-            call(["java", "Sud2sat", filename, filename+"_dimacs.txt"])
-            call(["minisat", filename+"_dimacs.txt", filename+"_satout.txt"])
-            call(["python3", "sat2sud.py", filename+"_satout.txt", filename+"_solvedBoard.txt"])
-        filename = line
-        filename = filename.strip().replace(" ", "") + ".txt"
-        currentFile = open(filename, "w")
-    else:
-        currentFile.write(line)
+            extract(filename)
+        else:
+            filename = infile + "_" + str(puzzle)
+            currentFile = open(filename+".txt", "w")
+            currentFile.write(line)
+            currentFile.close()
+            extract(filename)
+            puzzle += 1
 
-currentFile.close()
-file.close()
+    file.close()
 
-#Do it on last puzzle
-call(["java", "Sudoku", filename, filename+"_dimacs.txt"])
-call(["minisat", filename+"_dimacs.txt", filename+"_satout.txt"])
-call(["python3", "sat2sud.py", filename+"_satout.txt", filename+"_solvedBoard.txt"])
+def extract(filename):
+    call(["javac", "Sud2sat.java"])
+    call(["java", "Sud2sat", filename+".txt", filename+"_dimacs.txt"])
+    call(["minisat", filename+"_dimacs.txt", filename+"_satout.txt"])
+    call(["python3", "sat2sud.py", filename+"_satout.txt", filename+"_solvedBoard.txt"])
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
